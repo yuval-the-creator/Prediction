c0f363f08a174fe08cfe5d709a0de996ebffa697
yuval-the-creator <32yuval32@gmail.com>
2025-09-22 16:45:23 +0300

    add number of soldiers that extended their

--- generate_data.py
+++ generate_data.py
@@ -11,6 +11,7 @@ data = []
 for dep in departments:
     type1_employees = np.random.randint(100, 1000, size=len(months))
     type2_employees = np.random.randint(20, 500, size=len(months))
+    employees_to_extend = np.random.randint(10, 50, size=len(months))
     data.append(
         pd.DataFrame(
             {
@@ -18,6 +19,7 @@ for dep in departments:
                 "month": months,
                 "type1_employees": type1_employees,
                 "type2_employees": type2_employees,
+                "employees_to_extend": employees_to_extend,
             }
         )
     )
@@ -27,8 +29,10 @@ df = pd.concat(data, ignore_index=True)
 df = df.set_index(["department", "month"]).sort_index()
 
 # create history columns for each department
-for lag in range(1, 13):  # 1..12
+for lag in range(1, 13):
     df[f"type1_hist_{lag}"] = df.groupby(level=0)["type1_employees"].shift(lag)
     df[f"type2_hist_{lag}"] = df.groupby(level=0)["type2_employees"].shift(lag)
+    df[f"extend_hist_{lag}"] = df.groupby(level=0)["employees_to_extend"].shift(lag)
 
+df.drop("employees_to_extend", axis=1, inplace=True)
 df.dropna(inplace=True)
